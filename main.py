cd7baccf2ad7e31f0df9cd0a5c201b5e2a8a7962
Nahvin <vinson.han21@gmail.com>
2024-10-29 10:22:43 -0700

    rotating

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 import pygame
-import os
 from constants import *
 from player import Player
 
 
 def main():
-    """if "DISPLAY" in os.environ:  # Run audio only if DISPLAY is set
-        pygame.mixer.init()
-    else:
-        print("Audio initialization skipped in headless mode.")"""
-    
-    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-
-
-
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
